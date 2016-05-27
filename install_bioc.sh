f1dbda1f327e097dcdaac8410fa7de2c90191b01
Khagay Nagdimov <khagay@nyu.edu>
2016-05-27 12:26:26 -0400

    pass Travis

--- install_bioc.sh
+++ install_bioc.sh
@@ -3,8 +3,12 @@
 Rscript -e 'source("https://bioconductor.org/biocLite.R"); 
             biocLite("BiocInstaller"); 
             biocLite("Rsamtools");
+            library(Rsamtools);
+  
             biocLite("rtracklayer");
+            library(rtracklayer);
             biocLite("GenomicAlignments");
+            library(GenomicAlignments);
             biocLite("BSgenome.Hsapiens.UCSC.hg19");
             biocLite("GenomicRanges"); 
             install.packages("devtools"); 
