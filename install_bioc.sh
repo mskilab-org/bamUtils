71e19d302ddef7c9131a6dd33c3739502cbbceb4
Khagay Nagdimov <Crtemois612@gmail.com>
2016-05-31 13:56:15 -0400

    update install_bioc.sh

--- install_bioc.sh
+++ install_bioc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 Rscript -e 'source("https://bioconductor.org/biocLite.R"); biocLite("BiocInstaller"); biocLite("Rsamtools"); biocLite("openssl");
-biocLite("rtracklayer");biocLite("GenomicAlignments");library(openssl);library(Rsamtools);library(rtracklayer);library(GenomicAlignments);
+biocLite("rtracklayer");biocLite("GenomicAlignments");library(openssl);library(Rsamtools);
 biocLite("BSgenome.Hsapiens.UCSC.hg19");biocLite("GenomicRanges"); install.packages("devtools"); devtools::install_github("mskilab/gUtils"); 
 devtools::install_github("jimhester/covr");'
