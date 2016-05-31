c218fc67c5299745ca2bb9fe410170ee253e3132
Khagay Nagdimov <khagay@nyu.edu>
2016-05-31 17:00:59 -0400

    edit install_bioc

--- install_bioc.sh
+++ install_bioc.sh
@@ -3,4 +3,4 @@
 Rscript -e 'source("https://bioconductor.org/biocLite.R"); biocLite("BiocInstaller"); biocLite("Rsamtools"); biocLite("openssl");
 biocLite("rtracklayer");biocLite("GenomicAlignments");library(openssl);library(Rsamtools);library(rtracklayer);library(GenomicAlignments);
 biocLite("BSgenome.Hsapiens.UCSC.hg19");biocLite("GenomicRanges"); install.packages("devtools"); devtools::install_github("mskilab/gUtils"); 
-devtools::install_github("jimhester/covr");devtools::install_github("mskilab/skitools")'
+devtools::install_github("jimhester/covr");devtools::install_github("mskilab/skitools");library(skitools);'
